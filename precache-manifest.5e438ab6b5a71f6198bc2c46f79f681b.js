86bb600079c15ca57751bf5e33bff8bdd47ace87
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-23 07:24:19 +0100

    Updates

---
+++ precache-manifest.5e438ab6b5a71f6198bc2c46f79f681b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901d25cfd05b9bf0ff8a6fe0300ddeec",
+    "url": "/Meme_Generator/index.html"
+  },
+  {
+    "revision": "c8ebb478b7bae9735b09",
+    "url": "/Meme_Generator/static/css/main.449d934e.chunk.css"
+  },
+  {
+    "revision": "e920c686109421783f60",
+    "url": "/Meme_Generator/static/js/2.0a5578cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme_Generator/static/js/2.0a5578cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ebb478b7bae9735b09",
+    "url": "/Meme_Generator/static/js/main.2defbd58.chunk.js"
+  },
+  {
+    "revision": "ce2ee5ae1b9583809379",
+    "url": "/Meme_Generator/static/js/runtime-main.73b9262e.js"
+  }
+]);
